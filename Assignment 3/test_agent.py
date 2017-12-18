d2ad3a89e7dd954c146d63287f2f3b89734aae8e
mh806 <mh806@tf.uni-freiburg.de>
2017-12-18 15:22:55 +0100

    Assignment 03 Solutions

--- Assignment 3/test_agent.py
+++ Assignment 3/test_agent.py
@@ -3,18 +3,30 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from random import randrange
+from transitionTable import TransitionTable
 # custom modules
 from utils     import Options, rgb2gray
 from simulator import Simulator
+from keras.models import Sequential
+from keras.models import model_from_json
 
 # 0. initialization
 opt = Options()
 sim = Simulator(opt.map_ind, opt.cub_siz, opt.pob_siz, opt.act_num)
+trans = TransitionTable(opt.state_siz, opt.act_num, opt.hist_len,
+                             opt.minibatch_size, opt.valid_size,
+                             opt.states_fil, opt.labels_fil)
 
 # TODO: load your agent
 # Hint: If using standard tensorflow api it helps to write your own model.py  
 # file with the network configuration, including a function model.load().
 # You can use saver = tf.train.Saver() and saver.restore(sess, filename_cpkt)
+json_file = open('model.json','r')
+model = json_file.read()
+json_file.close()
+ml = model_from_json(model)
+ml.load_weights("model.h5")
+print("Model loaded")
 
 agent =None
 
@@ -45,8 +57,18 @@ for step in range(opt.eval_steps):
         #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         # Hint: get the image using rgb2gray(state.pob), append latest image to a history 
         # this just gets a random action
-        action = randrange(opt.act_num)
-        state = sim.step(action)
+        #action = randrange(opt.act_num)
+        #state = sim.step(action)
+        trans.add_recent(epi_step, rgb2gray(state.pob).reshape(opt.state_siz))
+        x = trans.get_recent()
+        # Reshape
+        x_rows = int(np.sqrt(opt.state_siz))
+        x_cols = x_rows
+        x_samples = x.shape[0]
+        hist = opt.hist_len
+        x = x.reshape(x_samples, x_rows , x_cols, hist)
+        action = ml.predict(x, batch_size=32, verbose=0)
+        state = sim.step(np.argmax(action))
 
         epi_step += 1
 
